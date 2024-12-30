263f950d62bb04f566830caaefda65d03d8b8077
swapna <swapnasiri.com>
2024-12-30 14:27:04 +0530

    conversation added

--- 05-variables.sh
+++ 05-variables.sh
@@ -2,7 +2,7 @@
 
 echo "please enter your username::" # text entered here will be added as  value to variable
 
-read USERNAME  # here USERNAME is the variable name
+read  -s USERNAME  # here USERNAME is the variable name
 
 echo "username entered: $USERNAME"
 
