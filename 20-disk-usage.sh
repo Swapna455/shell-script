f2d9a0697a0dc39f645b2934c16d8373388a5d72
swapna <swapnasiri.com>
2025-01-07 14:21:35 +0530

    shell-script

--- 20-disk-usage.sh
+++ 20-disk-usage.sh
@@ -2,10 +2,17 @@
 
 DISK_USAGE=$( df -hT | grep xfs)
 DISK_THRESHOLDS=5 # real project will monitor for 70
+MSG=""
 
 while read -r line
 do
    USAGE=$( echo $line | awk  -F " "  '{print $6F}' | cut -d "%" -f1 ) 
    PARTITION=$(echo $line | awk -F " " '{print $NF}')
-   echo "partition:  $PARTITION , Usage:  $USAGE"     
+   #echo "partition:  $PARTITION , Usage:  $USAGE" 
+   if [ $USAGE  -ge $DISK_THRESHOLD]
+   then
+       MSG="High Disk usage on partition: $PARTITION Usage is : $USAGE"
+
 done <<<$DISK_USAGE
+
+echo  "message: $MSG "
