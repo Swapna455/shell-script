02081b942a33e659d5bcf9b3ed0779de3b6dc54b
swapna <swapnasiri.com>
2025-01-07 14:13:40 +0530

    shell-script

--- 20-disk-usage.sh
+++ 20-disk-usage.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 DISK_USAGE=$( df -hT | grep xfs)
+DISK_THRESHOLDS=5 # real project will monitor for 70
 
 while read -r line
 do
-  echo $line
+   USAGE=$( echo $line | awk  -F " "  '{print $6F}' ) 
+   PARTITION=$(echo $line | awk -F " " '{print $NF}')
+   echo "partition:  $PARTITION , Usage:  $USAGE"     
 done <<<$DISK_USAGE
